7e44670b51f814b6d40ae5a3532b95f472d2c10d
Yuleson <yuleson@foxmail.com>
2016-06-27 12:22:10 +0800

    yu
    bat2

--- src/testFor/TestForBat.java
+++ src/testFor/TestForBat.java
@@ -1,10 +1,25 @@
 package testFor;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class TestForBat {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String path = "D:\\tracert.bat";
+		Runtime rt= Runtime.getRuntime();
+		try {
+			Process p = rt.exec("cmd.exe /k start " + path);
+		    InputStream in = p.getInputStream();
+            while (in.read() != -1) {
+                System.out.println(in.read());
+            }
+            in.close();
+            p.waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				
 	}
 
 }
